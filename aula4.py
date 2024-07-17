7355d79bd9d1f114790ebfa592f95973cde957dc
José Lucas da Silva Dantas <162215028+uluscaz-ifrn@users.noreply.github.com>
2024-07-17 14:27:02 +0000

    Correções

--- aula4.py
+++ aula4.py
@@ -1,7 +1,7 @@
 #Faça um algoritmo para calcular a média aritmética entre dois valores fornecidos pelo usuário e exiba  o resultado.
 """""
 valor1 = float(input("Digite o primeiro valor: "))
-valor2 = valor1 = float(input("Digite o primeiro valor: "))
+valor2 = float(input("Digite o segundo valor: "))
 media = (valor1+valor2)/2
 print(media)
 """ ""
