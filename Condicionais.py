b865eee99893f8f362a903aea4d23540f538f3bc
José Lucas da Silva Dantas <162215028+uluscaz-ifrn@users.noreply.github.com>
2024-07-18 11:19:27 +0000

    Estruturas Condicionais

---
+++ Condicionais.py
@@ -0,0 +1,6 @@
+a = int(input("Digite o primeiro valor: "))
+b = int(input("Digite o segundo valor: "))
+if a > b:
+  print("O primeiro valor é maior")
+if b > a:
+  print("O segundo valor é maior")
