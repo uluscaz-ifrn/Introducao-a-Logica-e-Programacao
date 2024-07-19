39fab659430b493b77567fa6c2787edb3914501e
José Lucas da Silva Dantas <162215028+uluscaz-ifrn@users.noreply.github.com>
2024-07-19 10:25:56 +0000

    Uso do Else

--- Aulas/Condicionais.py
+++ Aulas/Condicionais.py
@@ -4,3 +4,8 @@ if a > b:
   print("O primeiro valor é maior")
 if b > a:
   print("O segundo valor é maior")
+
+if a == b:
+  print("Os valores são iguais")
+else:
+  print("Os valores são diferentes")
