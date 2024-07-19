a2edcc01bc2b174762054d31a7f162ceaa6cfae9
José Lucas da Silva Dantas <162215028+uluscaz-ifrn@users.noreply.github.com>
2024-07-19 10:34:56 +0000

    Exemplos

--- Aulas/Condicionais.py
+++ Aulas/Condicionais.py
@@ -1,3 +1,4 @@
+"""
 a = int(input("Digite o primeiro valor: "))
 b = int(input("Digite o segundo valor: "))
 if a > b:
@@ -9,3 +10,9 @@ if a == b:
   print("Os valores são iguais")
 else:
   print("Os valores são diferentes")
+"""
+a = int(input("Digite o primeiro valor: "))
+if a > 10 and a < 20:
+  print("O valor está entre 10 e 20")
+else:
+  print("O valor não está entre 10 e 20")
