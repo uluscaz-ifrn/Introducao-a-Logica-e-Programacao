36c60b49aee02cf0e5d9bf9999d5c27b46404127
José Lucas da Silva Dantas <162215028+uluscaz-ifrn@users.noreply.github.com>
2024-07-19 10:51:14 +0000

    Atividade uso dos Elif's

---
+++ Atividades/Atividade6.py
@@ -0,0 +1,17 @@
+1 = 10
+2 = 18
+3 = 23
+4 = 28
+5 = 31
+
+preco_categoria = int(input("Informe a categoria do produto: "))
+if preco_categoria == 1:
+  print(f"O preço do produto é R$ {1}")
+elif preco_categoria == 2
+  print(f"O preço do produto é R$ {2}")
+elif preco_categoria == 3
+print(f"O preço do produto é R$ {3}")
+elif preco_categoria == 4
+print(f"O preço do produto é R$ {4}")
+elif preco_categoria == 5
+print(f"O preço do produto é R$ {5}")
