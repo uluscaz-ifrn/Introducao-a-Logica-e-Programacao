9c7ebc45e3682eaa743a5d22d084ea8f631cb968
José Lucas da Silva Dantas <162215028+uluscaz-ifrn@users.noreply.github.com>
2024-07-19 11:08:34 +0000

    Melhorias na estrutura

--- Atividades/Atividade6.py
+++ Atividades/Atividade6.py
@@ -2,12 +2,16 @@ categoria = int(input("Informe a categoria do produto: "))
 preco = 0
 if categoria == 1:
   preco = 10
-if categoria == 2:
-  preco = 18
-if categoria == 3:
-  preco = 23
-if categoria == 4:
-  preco = 26
-if categoria == 5:
-  preco = 31
+else:
+  if categoria == 2:
+    preco = 18
+  else:
+    if categoria == 3:
+      preco = 23
+    else:
+      if categoria == 4:
+        preco = 26
+      else:
+        if categoria == 5:
+          preco = 31
 print("O preço do produto é: R$", preco)
