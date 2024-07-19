7ff88c3b2ea11a99d4eb5a9257441e6b8f16bfd8
José Lucas da Silva Dantas <162215028+uluscaz-ifrn@users.noreply.github.com>
2024-07-19 10:40:40 +0000

    Exemplo com "OR"

--- Aulas/Condicionais.py
+++ Aulas/Condicionais.py
@@ -10,9 +10,15 @@ if a == b:
   print("Os valores são iguais")
 else:
   print("Os valores são diferentes")
-"""
+
 a = int(input("Digite o primeiro valor: "))
 if a > 10 and a < 20:
   print("O valor está entre 10 e 20")
 else:
   print("O valor não está entre 10 e 20")
+"""
+a = int(input("Digite o primeiro valor: "))
+if a == 0 or a == 1:
+  print("O valor é binário")
+else:
+  print("O valor não é binário")
