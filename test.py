5dc54fcb8526079f4385d698af7a09b5029e859c
José Lucas da Silva Dantas <162215028+uluscaz-ifrn@users.noreply.github.com>
2024-07-25 08:39:29 -0300

    Create test.py
    prova 2

---
+++ test.py
@@ -0,0 +1,11 @@
+n1 = int(input("Digite um número: "))
+n2 = int(input("Digite outro número: "))
+n3 = int(input("Digite mais um número: "))
+print(f"Os números informados são: {n1}, {n2}, {n3}")
+if n1 < n2:
+    n1, n2 = n2, n1
+elif n1 < n3:
+    n1, n3 = n3, n1
+elif n2 < n3:
+    n2, n3 = n3, n2
+print(f"Os números em ordem decrescente são: {n1}, {n2}, {n3}")
