a61cf40fd340d32087bde51ec72c3482f226c7d8
Lucas Dantas <162215028+uluscaz-ifrn@users.noreply.github.com>
2024-08-06 23:23:01 +0000

    Remoção de algumas atividades para serem colocadas em outra pasta.

--- Atividades/Atividade11.py
+++
@@ -1,9 +0,0 @@
-idade = int(input("Digite a idade: "))
-classe_eleitoral = ""
-if idade < 16:
-  classe_eleitoral = "Não eleitor"
-elif idade >= 18 and idade <= 65:
-  classe_eleitoral = "Eleitor obrigatório"
-elif idade >= 16 and idade <= 18 or idade > 65:
-  classe_eleitoral = "Eleitor facultativo"
-print(classe_eleitoral)
