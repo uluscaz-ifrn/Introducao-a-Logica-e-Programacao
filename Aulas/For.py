cb558d39a7db8fdad3331e1832d4ca5d734b3854
Lucas Dantas <162215028+uluscaz-ifrn@users.noreply.github.com>
2024-08-08 10:52:16 +0000

    Uso for com um exemplo

--- Aulas/For.py
+++ Aulas/For.py
@@ -1,2 +1,10 @@
-for i in range(10):
-  print(i)
+#for i in range(10):
+#  print(i)
+total_geral = 0
+
+for i in range(1, 4):
+  qts_acai = int(input("Digite a quantidade de açaís vendidos nos últimos %d atrás" %i))
+  total = qts_acai * 3.00
+  print("Total vendido: R$ %2.f de açaí" %total)
+  total_geral = total_geral + total
+print("Foram vendidos R$ %2.f de açaí nos últimos %d dias" %(total_geral, i))
