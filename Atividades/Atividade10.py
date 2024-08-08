dea81267deb184cdecfbb89da61ab190f61f5a17
Lucas Dantas <162215028+uluscaz-ifrn@users.noreply.github.com>
2024-08-08 11:06:04 +0000

    Exercícios utilizando o For

---
+++ Atividades/Atividade10.py
@@ -0,0 +1,3 @@
+#Desenvolva um programa que mostre todos os números de 200 a 1.
+for i in range(200, 0, -1):
+  print(i)
---
+++ Atividades/Atividade9.py
@@ -0,0 +1,3 @@
+#Desenvolva um programa que mostre todos os números de 1 a 100.
+for i in range(1, 101):
+  print(i)
